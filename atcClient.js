146ac3c30af30768fa34fba40f5a9074e1e616cc
Pedro Rodrigues <prodrigues1990@gmail.com>
2017-08-11 23:36:51 +0100

    ATC towers via JS ES6 classes

---
+++ atcClient.js
@@ -0,0 +1,51 @@
+class atcClient {
+  constructor(client, map) {
+
+    // retain raw data
+    this.__client = client;
+
+    this.id = client['_id'];
+    this.callsign = client['callsign'];
+    this.realname = client['realname'];
+
+    // ATC types:
+    //            0 : observer
+    //            1 : flight service station
+    //            2 : delivery
+    //            3 : ground
+    //            4 : tower/atis
+    //            5 : approach
+    //            6 : center
+    this.facility_type = (client['facilitytype'] == 0) ? 'observer' :
+                        (client['facilitytype'] == 1) ? 'fss' :
+                        (client['facilitytype'] == 2) ? 'delivery' :
+                        (client['facilitytype'] == 3) ? 'ground' :
+                        (client['facilitytype'] == 4) ? 'tower' :
+                        (client['facilitytype'] == 5) ? 'approach' :
+                        (client['facilitytype'] == 6) ? 'center' :
+                        'unknown';
+
+    // convert to correct format for google maps
+    // TODO: consider LatLng class
+    this.location = {
+                      lat: client['location'][1],
+                      lng: client['location'][0]
+                    };
+
+    // initiate marker object
+    if (this.facility_type == 'delivery' ||
+        this.facility_type == 'ground' ||
+        this.facility_type == 'tower') {
+
+          this.map_marker = new google.maps.Marker({
+            position: this.location,
+            map: map,
+            icon: '../airport-icon.png',
+            title: this.callsign + ' ' + this.realname
+          });
+      } else {
+        this.map_marker = null;
+      }
+
+  };
+}
