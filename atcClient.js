8604b0c3204c97d6a9e4e7d4465de61a0a45a09b
Tiago Vicente <tmavicente@gmail.com>
2019-10-08 23:43:11 +0100

    add approach

--- atcClient.js
+++ atcClient.js
@@ -44,7 +44,22 @@ class atcClient {
             title: this.callsign + ' ' + this.realname,
             zIndex: 10000 + this.__client['facilitytype']
           });
-      } else {
+      } if(this.facility_type == 'approach'){
+
+        this.map_marker = new google.maps.Circle({
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35,
+          map: map,
+          title: this.callsign + ' ' + this.realname,
+          center: new google.maps.LatLng(this.location.lat,
+            this.location.lng),
+          radius: 100000
+        });
+
+    } else {
         this.map_marker = null;
       }
 
